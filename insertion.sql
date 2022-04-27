9cb67499815d955f6dd98284c336643505453789
chamsTmar1 <chamstmar@gmail.com>
2022-04-27 23:39:41 +0100

    bd insertion

---
+++ insertion.sql
@@ -0,0 +1,15 @@
+INSERT INTO `Warari`.`Compte` VALUES('123','Salma Zarrouk','salmazk42@gmail.com','mouradghail@gmail.com','test');
+INSERT INTO `Warari`.`Compte` VALUES('456','Dhafer Landolsi','dhaferlandolsi@gmail.com','marwajemli@gmail.com','test');
+INSERT INTO `Warari`.`Compte` VALUES('789','Dorra Sabri','dorrasabri20@yahoo.fr','medboulila@gmail.com','test');
+INSERT INTO `Warari`.`Compte` VALUES('101','Marwen Ben Ali','marwenbenali@hotmail.com','zainebferchichi@gmail.com','test');
+INSERT INTO `Warari`.`Compte` VALUES('112','Ala Mejri','alamejri@retica.io','mariembenamor@gmail.com','test');
+INSERT INTO `Warari`.`Salle` VALUES('123','Regency','Salle des Fêtes','22.568.489','La Marsa');
+INSERT INTO `Warari`.`Salle` VALUES('456','Terrasse Les Cascades - Ecovillage','Espace Plein-air','98.560.201','Sidi Bou Ali');
+INSERT INTO `Warari`.`Salle` VALUES('789','Espace Suprême - Forum El Afrah','Salle des Fêtes','23.550.333','Monastir');
+INSERT INTO `Warari`.`Salle` VALUES('101','Altesse','Espace Plein-air','99.400.100','La Marsa');
+INSERT INTO `Warari`.`Salle` VALUES('112','Naher El Founoun','Salle des Fêtes','26.314.412','Sfax-Ville');*/
+INSERT INTO `Warari`.`Patisserie` VALUES('123','Moni Bk','Tunis','22.568.489');
+INSERT INTO `Warari`.`Patisserie` VALUES('456','Belkadhi','Sidi Bou Ali','98.560.201');
+INSERT INTO `Warari`.`Patisserie` VALUES('789','Paon d\'Or','Monastir','23.550.333');
+INSERT INTO `Warari`.`Patisserie` VALUES('101','Beb Eddiwen','Ben Arous','99.400.100');
+INSERT INTO `Warari`.`Patisserie` VALUES('112','Mme Rekik','Sfax-Ville','26.314.412');
